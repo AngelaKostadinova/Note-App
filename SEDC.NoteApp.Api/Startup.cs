6a418f24ccdab4aa6e0b12c55b44fddb67a43a60
AngelaKostadinova <angelakostadinova7@gmail.com>
2020-11-12 17:37:12 +0100

    Note app update

---
+++ SEDC.NoteApp.Api/Startup.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using SEDC.NoteApp.Services;
+using SEDC.NoteApp.Services.Helpers;
+
+namespace SEDC.NoteApp.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var appConfig = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appConfig);
+
+            var appSettings = appConfig.Get<AppSettings>();
+            string connectionString = appSettings.NoteAppConnectionString;
+
+            DIModule.RegisterModule(services, connectionString);
+
+            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<INoteService, NoteService>();
+
+            //Jwt token authentiaction configuration
+            var secret = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(secret),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseAuthentication();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
---
+++ SEDC.NoteApp.Services/Helpers/DIModule.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SEDC.NoteApp.DataAccess;
+using SEDC.NoteApp.DataAccess.ADONET;
+using SEDC.NoteApp.DataAccess.DapperRepositories;
+using SEDC.NoteApp.DataAccess.EntityFramework;
+using SEDC.NoteApp.DataModels;
+
+namespace SEDC.NoteApp.Services.Helpers
+{
+    public static class DIModule
+    {
+        public static IServiceCollection RegisterModule(IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<NoteDbContext>(x => 
+                x.UseSqlServer(connectionString));
+
+            services.AddTransient<IRepository<UserDTO>, UserRepository>();
+            services.AddTransient<IRepository<NoteDTO>, NoteRepository>();
+
+            return services;
+        }
+    }
+}
